58051f141e7b51e6ef95402158d5c2a96417fdab
OMI786I <sohrawardy1998@gmail.com>
2025-01-29 14:33:42 +0600

    understanding styles application using layout or individually inside the component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4511e",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen
+        name="details2"
+        options={{
+          title: "My custom heading from layout for details2",
+          headerStyle: { backgroundColor: "black" },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
+        backgroundColor: "red",
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text
+        style={{
+          fontSize: 30,
+          color: "white",
+        }}
+      >
+        Hello
+      </Text>
+      <Link href={"/about"}>Go to about</Link>
+      <Link href={"/custom"}>Go to custom</Link>
+      <Link href={"/details2"}>Go to details</Link>
     </View>
   );
 }
